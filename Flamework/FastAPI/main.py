0949cf49dd39218262ec3dbabf86be1921a73030
KimTaekGwan <sbe07032@naver.com>
2023-02-06 17:37:33 +0900

    fastapi

---
+++ Flamework/FastAPI/main.py
@@ -0,0 +1,10 @@
+# pip install fastapi
+# pip install "uvicorn[standard]"
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
